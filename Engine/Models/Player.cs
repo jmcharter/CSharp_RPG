4a9342736eef7a1bf34c8cb1fac9846d55672803
Jon <jmcharter@gmail.com>
2020-06-21 22:21:27 +0100

    changes to allow update of experience level binding on button press

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
+        private int _getExperiencePoints;
+
         public string Name { get; set; }
         public string CharacterClass { get; set; }
         public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
+        public int ExperiencePoints 
+        { 
+            get { return _getExperiencePoints; }
+            set { 
+                _getExperiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            } 
+        }
         public int Level { get; set; }
         public int Gold { get; set; }
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
