948e3beb8521b58f3891a6f32dd866c51bd6b027
Jon <jmcharter@gmail.com>
2020-06-21 23:43:22 +0100

    Home image and binding added. Location model setup referenced in view

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,16 +10,25 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Jon";
-            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.Gold = 500;
             CurrentPlayer.CharacterClass = "Hunter";
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house.";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+
         }
     }
 }
